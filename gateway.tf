260cdc4342b30c76cb3018c6b9d1b79599873460
siddhu <siddhudangeti@gmail.com>
2022-01-18 23:55:48 +0530

    route-tables associations

--- gateway.tf
+++ gateway.tf
@@ -6,6 +6,11 @@ resource "aws_internet_gateway" "gw" {
     ENV  = var.ENV
   }
 }
+resource "aws_route" "route-public" {
+  route_table_id            = aws_route_table.public-route.id
+  destination_cidr_block    = "0.0.0.0/0"
+  gateway_id                = aws_internet_gateway.gw.id
+}
 
 #resource "aws_nat_gateway" "example" {
 #  allocation_id =

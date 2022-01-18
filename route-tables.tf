5fe47e2a8aba6d393234d57de09a9b3f0bf97e08
siddhu <siddhudangeti@gmail.com>
2022-01-18 23:22:23 +0530

    route-tables associations

--- route-tables.tf
+++ route-tables.tf
@@ -15,15 +15,14 @@ resource "aws_route_table" "Private-route" {
     ENV  = var.ENV
   }
 }
-
-resource "aws_route_table_association" "Public-assign" {
-  count          = length(aws_subnet.Public_subnets)
+resource "aws_route_table_association" "Public" {
+  count = length(aws_subnet.Public_subnets)
   subnet_id      = aws_subnet.Public_subnets.*.id
   route_table_id = aws_route_table.public-route.id
 }
 
-resource "aws_route_table_association" "private-assign" {
-  count          = length(aws_subnet.Private_subnets)
+resource "aws_route_table_association" "Private" {
+  count = length(aws_subnet.Private_subnets)
   subnet_id      = aws_subnet.Private_subnets.*.id
   route_table_id = aws_route_table.Private-route.id
 }

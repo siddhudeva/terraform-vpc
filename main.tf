2e344580eacde6b54e6c7a9c836ba5df164936ff
siddhu <siddhudangeti@gmail.com>
2022-01-18 19:43:07 +0530

    subnet creation

--- main.tf
+++ main.tf
@@ -1,6 +1,20 @@
-resource "aws_vpc" "main" {
-  cidr_block       = var.VPC_CIDR
+
+resource "aws_subnet" "Public_subnets" {
+  count = lenght(var.PUBLIC_SUBNET_CIDR)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.PUBLIC_SUBNET_CIDR[count.index]
+
   tags = {
-    Name = var.ENV
+    Name = "Public_subnet"
+  }
+}
+
+resource "aws_subnet" "Private_subnets" {
+  count = length(var.PRIVATE_SUBNET_CIDR)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.PRIVATE_SUBNET_CIDR[count.index]
+
+  tags = {
+    Name = "private_subnet"
   }
 }

d59eab5ba2c920ec34984e240753ad0250960050
siddhu <siddhudangeti@gmail.com>
2022-01-18 22:33:25 +0530

    subnet creation

---
+++ route-tables.tf
@@ -0,0 +1,17 @@
+resource "aws_route_table" "public-route" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "Public_rt"
+    ENV  = var.ENV
+  }
+}
+
+resource "aws_route_table" "Private-route" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "private"
+    ENV  = var.ENV
+  }
+}

0920b424b2a4d88f264c7320a6599cf7b8d649aa
siddhu <siddhudangeti@gmail.com>
2022-01-19 10:30:31 +0530

    peering connection-routetables

--- route-tables.tf
+++ route-tables.tf
@@ -48,12 +48,12 @@ resource "aws_route" "route-private" {
   gateway_id             = aws_nat_gateway.ngw.id
 }
 
-resource "aws_route" "public-peer" {
+resource "aws_route" "public-peer-default" {
   route_table_id            = aws_route_table.public-route.id
   destination_cidr_block    = data.aws_vpc.default.cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection.peer-vpcs.id
 }
-resource "aws_route" "route-private" {
+resource "aws_route" "route-peer-main" {
   route_table_id            = aws_route_table.Private-route.id
   destination_cidr_block    = aws_vpc.main.cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection.peer-vpcs.id
